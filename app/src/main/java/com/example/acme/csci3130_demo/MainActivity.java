eb3adac67fcf3d4a80bbf5cf58a6aafd6698c6c5
Marlee Donnelly <mdonnelly@dal.ca>
2018-05-17 21:08:42 -0300

    Assignment 1, Question 3: Added a text field and button that enable the user to change the text in the middle of the screen.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //Send message when user presses send button
+    public void updateText(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+
+        //Set textview to extracted string
+        TextView resultText = findViewById(R.id.resultText);
+        resultText.setText(message);
+    }
+
 }
